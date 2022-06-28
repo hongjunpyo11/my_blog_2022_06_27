3f05e501be758a0f4d6c1dbafb950bd251e8fe8e
hongjunpyo11 <jpyo98@naver.com>
2022-06-28 16:16:08 +0900

    StaticImg 컴포넌트로 이미지 노출

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,16 +1,15 @@
 import React from "react";
-import Head from "../components/head";
+import Layout from "../components/Layout";
 
 function AboutPage() {
     return (
-        <main>
-            <title>자기소개</title>
+        <Layout pageTitle="About Me">
+            <p>
+                Hi there! I'm the proud creator of this site, which I built with Gatsby.
+            </p>
+        </Layout>
 
-            <Head />
-
-            <h1>자기소개 페이지 입니다.</h1>
-        </main>
     );
-}
+};
 
 export default AboutPage;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,16 @@
+import { StaticImage } from "gatsby-plugin-image";
 import React from "react";
-import Head from "../components/head";
+import Layout from "../components/Layout";
 
 const IndexPage = () => {
   return (
-    <main>
-      <title>Home Page</title>
-
-      <Head />
-
-      <h1>Home Page</h1>
-    </main>
+    <Layout pageTitle="Home Page">
+      <p>I'm making this by following the Gatsby Tutorial.</p>
+      <StaticImage
+        alt="Clifford, a reddish-brown pitbull, posing on a couch and looking stoically at the camera"
+        src="https://pbs.twimg.com/media/E1oMV3QVgAIr1NT?format=jpg&name=large"
+      />
+    </Layout>
   );
 };
 
