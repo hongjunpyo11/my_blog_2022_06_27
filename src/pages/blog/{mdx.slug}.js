1a71c0ecddc047e65f72475e9b498522af773b36
hongjunpyo11 <jpyo98@naver.com>
2022-06-28 20:32:10 +0900

    오류발생

---
+++ src/pages/blog/{mdx.slug}.js
@@ -0,0 +1,23 @@
+import React from "react";
+import Layout from "../../components/layout";
+const BlogPost = () => {
+    return (
+        <Layout pageTitle="Super Cool Blog Posts">
+            <p>My blog post contents will go here (eventually).</p>
+        </Layout>
+    );
+};
+
+export const query = graphql`
+  query ($id: String) {
+    mdx(id: {eq: $id}) {
+      frontmatter {
+        title
+        date(formatString: "MMMM D, YYYY")
+      }
+      body
+    }
+  }
+`;
+
+export default BlogPost;
